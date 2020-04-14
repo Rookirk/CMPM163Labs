892f371b4e814ca942abd1839260378ed33c03c9
Rookirk <hwu24110@gmail.com>
2020-04-13 23:17:10 -0700

    Camera now rotates around the scene

--- lab2/src/main.js
+++ lab2/src/main.js
@@ -1,23 +1,44 @@
 let scene, camera, renderer;
 let models = {};
 
+const cameraDist = 17;
+
 function main() {
 	scene = new THREE.Scene();
 
 	camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 0.1, 1000);
-	camera.position.z = 17;
+	camera.translateZ( cameraDist );
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	var light = new THREE.PointLight(0xffffff, 1, 1000);
-	light.position.set(50, 50, 50);
-	scene.add(light);
+	const pointLight = new THREE.PointLight(0xffffff, 1, 1000);
+	pointLight.position.set(50, 50, 50);
+	scene.add( pointLight );
+
+	const pointLight2 = new THREE.PointLight(0x633284, .5, 1000);
+	pointLight2.position.set(-50, -50, -50);
+	scene.add( pointLight2 );
 
 	myLoad('SexyClarke_Whole.glb', "Clarke", models, function(gltf){
 		gltf.scene.position.y = 3.4;
 		gltf.scene.position.x = -.2;
 		gltf.scene.rotation.y = -.35;
 	});
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+
+	//models.Clarke.rotation.y += .01;
+
+	cameraRotate();
+}
+
+function cameraRotate(){
+	camera.translateZ( -cameraDist );
+	camera.rotation.y -= .01;
+	camera.translateZ( cameraDist );
 }
