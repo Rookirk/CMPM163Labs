25946253ba4e22ba62b3e3902c56d14f06599ddf
Rookirk <hwu24110@gmail.com>
2020-04-19 00:11:22 -0700

    Added texture maps

--- lab3/src/main.js
+++ lab3/src/main.js
@@ -30,6 +30,10 @@ function main() {
 	light.position.set(10, 10, 10);
 	scene.add(light);
 
+	var light2 = new THREE.PointLight(0xffffff, 1, 1000);
+	light2.position.set(10, -10, 10);
+	scene.add(light2);
+
 	mainLoaded = true;
 	checkIfLoaded();
 }
@@ -37,34 +41,61 @@ function main() {
 function animate() {
 	requestAnimationFrame(animate);
 
-	cubeGeometry.rotateX(0.01);
+	cubeGeometry.rotateX(0.02);
 	cubeGeometry.rotateY(0.01);
 
 	renderer.render(scene, camera);
 }
 
 function buildScene() {
-	const phongMaterial = new THREE.MeshPhongMaterial( {
-		color: 0xdddddd,
+	const phongMaterial1 = new THREE.MeshPhongMaterial( {
+		color: 0xFF9BF1,
 		specular: 0x00ff00,
 		shininess: 30,
-		emissive: 0x00FFFF });
+	});
 
-	const uniforms = {
-		colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
-		colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
-	};
+	// bump map used from:
+	// http://www.bundysoft.com/wiki/doku.php?id=tutorials:l3dt:bumpmap
+	const phongMaterial2 = new THREE.MeshPhongMaterial( {
+		color: 0xFF9BF1,
+		specular: 0x00ff00,
+		shininess: 30,
+		bumpMap: new THREE.TextureLoader().load( 'maps/bumpMap.jpg'),
+	});
 
-	const shaderMaterial =  new THREE.ShaderMaterial({
-		uniforms: uniforms,
+	const shaderMaterial1 =  new THREE.ShaderMaterial({
+		uniforms: {
+			colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+			colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+		},
 		fragmentShader: shaders.f,
 		vertexShader: shaders.v,
-		precision: "mediump"});
+		precision: "mediump"
+	});
 
-	addCube([-1,-1,0], phongMaterial);
-	addCube([1,-1,0], shaderMaterial);
-	addCube([-1,1,0], phongMaterial);
-	addCube([1,1,0], shaderMaterial);
+	// I generated these textures with Substance Painter
+	const phongMaterial3 = new THREE.MeshPhongMaterial( {
+		map: new THREE.TextureLoader().load( 'maps/AlbedoMaterial.png' ),
+		bumpMap: new THREE.TextureLoader().load( 'maps/HeightMaterial.png'),
+		emissiveMap: new THREE.TextureLoader().load( 'maps/EmissiveMaterial.png'),
+		normalMap: new THREE.TextureLoader().load( 'maps/NormalMaterial.png')
+	});
+
+	const shaderMaterial2 =  new THREE.ShaderMaterial({
+		uniforms: {
+			colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+			colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+		},
+		fragmentShader: shaders.f,
+		vertexShader: shaders.v,
+		precision: "mediump"
+	});
+
+	addCube([-1,-1,0], phongMaterial1);
+	addCube([-3,-1,0], phongMaterial2);
+	addCube([1,-1,0], shaderMaterial1);
+	addCube([-1,1,0], phongMaterial3);
+	addCube([1,1,0], shaderMaterial2);
 }
 
 function addCube( position, material ) {
