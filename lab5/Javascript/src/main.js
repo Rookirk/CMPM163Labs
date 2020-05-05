211649efa61cb18746055d23db4863114c55cfb0
Rookirk <hwu24110@gmail.com>
2020-05-05 00:32:59 -0700

    particles now change colors

--- lab5/Javascript/src/main.js
+++ lab5/Javascript/src/main.js
@@ -1,12 +1,15 @@
 const shaders = {}, textures = {};
 
 let scene, camera, renderer, light;
-let cubGeometry;
+let particleMat;
 
 function main() {
 	THREE.Cache.enabled = true;
 
 	initLoaders();
+
+	loadTexture( "textures/disc.png", 'disc', textures );
+
 	// setup the scene
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -15,6 +18,19 @@ function main() {
 	document.body.appendChild(renderer.domElement);
 	camera.position.z = 60;
 
+	mainIsLoaded();
+}
+
+function animate() {
+	var time = Date.now() * 0.00005;
+	var h = (360 * (1.0 + time) % 360) / 360;
+	particleMat.color.setHSL(h, 0.5, 0.5);
+
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
+}
+
+function buildScene() {
 	var vertices = [];
 
 	for ( var i = 0; i < 1000; i ++ ) {
@@ -28,19 +44,9 @@ function main() {
 	geometry.setAttribute( 'position', new 
 	THREE.Float32BufferAttribute(vertices, 3));
 
-	var material = new THREE.PointsMaterial( { color: 0xef983e } );
+	particleMat = new THREE.PointsMaterial( { size: 20, sizeAttenuation: false, map: textures.disc, alphaTest: 0.5, transparent: true } );
+	particleMat.color.setHSL( 1.0, 0.3, 0.7 );
 
-	var points = new THREE.Points(geometry, material);
+	var points = new THREE.Points(geometry, particleMat);
 	scene.add(points);
-
-	mainIsLoaded();
-}
-
-function animate() {
-	requestAnimationFrame(animate);
-	renderer.render(scene, camera);
-}
-
-function buildScene() {
-
 }
