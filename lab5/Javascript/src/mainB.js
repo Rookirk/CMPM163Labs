beed6d04c95367119ac585e4efe3ddbfdcd2ff81
Rookirk <hwu24110@gmail.com>
2020-05-05 15:59:58 -0700

    Particle explosion is now spherical

--- lab5/Javascript/src/mainB.js
+++ lab5/Javascript/src/mainB.js
@@ -6,6 +6,7 @@ function mainB() {
 
 	const main = new Project(
 		function(main){
+			main.loadTexture( "textures/disc.png", 'disc');
 			// setup the scene
 			scene = new THREE.Scene();
 			camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -28,20 +29,23 @@ function mainB() {
 			particles = [];
 			const geo = new THREE.Geometry();
 			for(let i=0; i<1000; i++) {
+				const randAngle = Math.random() * Math.PI*2;
+				const randZ = Math.random() * 2 - 1
 				const particle = {
 					position: new THREE.Vector3(
-						Math.random() * 2 - 1,
+						Math.random() * 2 - 65,
 						Math.random() * 2 - 1,
 						Math.random() * 3 - 3
 					),
+					//https://math.stackexchange.com/questions/44689/how-to-find-a-random-axis-or-unit-vector-in-3d
 					velocity: new THREE.Vector3(
-						Math.random() * .02 - .01,
-						0.06,
-						Math.random() * .02 - .01
+						Math.sqrt(1 - Math.pow(randZ,2)) * Math.cos(randAngle) * .5 + 1.1,
+						Math.sqrt(1 - Math.pow(randZ,2)) * Math.sin(randAngle) * .4 + .1,
+						randZ * .4 - .01
 					),
 					acceleration: new THREE.Vector3(
-						Math.random() * .002 - .001, 
-						Math.random() * .002 - .001, 
+						0, 
+						Math.random() * .004 - .008, 
 						0
 					),
 				}
@@ -49,7 +53,7 @@ function mainB() {
 				geo.vertices.push(particle.position)
 			}
 
-			const particleMat = new THREE.PointsMaterial({color:0xffffff,size: 0.5});
+			const particleMat = new THREE.PointsMaterial({color:0xffffff, size: 0.5, map: textures.disc, transparent: true });
 			mesh = new THREE.Points(geo,particleMat);
 			mesh.position.z = -4;
 			scene.add(mesh);
