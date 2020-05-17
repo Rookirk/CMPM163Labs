475f5eeaf69947029040054c33f231c3e9012247
Rookirk <hwu24110@gmail.com>
2020-05-17 00:55:12 -0700

    Started part2B with wavy plane

--- lab7/Javascript/src/mainB.js
+++ lab7/Javascript/src/mainB.js
@@ -1,6 +1,8 @@
-let scene, camera, renderer;
+let scene, camera, renderer, matShader;
 
 function mainB() {
+
+	const startTime = Date.now();
 
 	const main = new Project(
 		function(main){
@@ -14,6 +16,11 @@ function mainB() {
 		},
 
 		function(main){
+			const time = startTime - Date.now();
+			if(matShader){
+				matShader.uniforms.time.value = time/500;
+			}
+
 			renderer.render(scene, camera);
 		},
 
@@ -26,6 +33,33 @@ function mainB() {
 			light2.position.set(0,5,-25);
 			scene.add(light2);
 
+			const mat = new THREE.MeshPhongMaterial({color:0x2288ff, shininess:100, side:THREE.DoubleSide})
+			const p = new THREE.PlaneGeometry(20, 20, 100, 100);
+			const plane = new THREE.Mesh(p, mat);
+			plane.position.z = -20;
+			plane.rotation.x = -1.2;
+			scene.add(plane);
+
+			mat.onBeforeCompile = (shader) => {
+				shader.uniforms.time = { value: 0};
+				shader.vertexShader = `
+				        uniform float time;
+				    ` + shader.vertexShader;
+				
+				const token = '#include <begin_vertex>'
+				const customTransform = `
+				        vec3 transformed = vec3(position);
+				        float freq = 3.0;
+				        float amp = 0.1;
+				        float angle = (time + position.x)*freq;
+				        transformed.z += sin(angle)*amp;
+
+				        objectNormal = normalize(vec3(-amp * freq * cos(angle),0.0,1.0));
+						vNormal = normalMatrix * objectNormal;
+				`
+				shader.vertexShader = shader.vertexShader.replace(token,customTransform);
+				matShader = shader;
+			}
 		}
 	);
 
