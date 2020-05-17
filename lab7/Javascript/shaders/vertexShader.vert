49d6abed98fa50ae7b0c068e1f0164ee1a81cc15
Rookirk <hwu24110@gmail.com>
2020-05-16 18:21:57 -0700

    Added height map

--- lab7/Javascript/shaders/vertexShader.vert
+++ lab7/Javascript/shaders/vertexShader.vert
@@ -1,7 +1,11 @@
+uniform sampler2D heightMap1;
+
 varying vec2 vUv;
 
 void main() {
 	vUv = uv;
-	vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+	vec4 heightColor = texture2D(heightMap1, uv);
+	vec3 newPosition = position + normal * 1.0 * heightColor.r;
+	vec4 modelViewPosition = modelViewMatrix * vec4(newPosition, 1.0);
 	gl_Position = projectionMatrix * modelViewPosition;
 }
--- lab7/Javascript/src/mainA.js
+++ lab7/Javascript/src/mainA.js
@@ -7,6 +7,9 @@ function mainA() {
 			main.loadShader("shaders/fragShader.frag", "frag");
 			main.loadShader("shaders/vertexShader.vert", "vert");
 
+			main.loadTexture("textures/images.jpg", "height");
+			main.loadTexture("textures/terrain_texture.jpg", "terrain");
+
 			// setup the scene
 			scene = new THREE.Scene();
 			camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -23,10 +26,28 @@ function mainA() {
 		},
 
 		function(main){
-			light = new THREE.DirectionalLight(0xffffff, 1.0);
+			const light = new THREE.DirectionalLight(0xffffff, 1.0);
 			light.position.set(1,1,1).normalize();
 			scene.add(light);
 			scene.add(new THREE.AmbientLight(0xffffff, 0.3));
+
+			const uniforms = {
+				texture1: { type: "t", value: textures.terrain },
+				heightMap1: { type: "t",value: textures.height }
+			};
+
+			const material = new THREE.ShaderMaterial({
+				uniforms: uniforms, 
+				vertexShader: shaders.vert,
+				fragmentShader: shaders.frag
+			});
+
+			const plane = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 200, 200), material);
+			plane.position.z = -10;
+			plane.position.y = -1;
+			plane.rotation.x = -Math.PI / 2;
+			scene.add(plane);
+
 		}
 	);
 
