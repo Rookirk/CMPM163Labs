92a05d67cf3541024b533fc931dfd66713b336f5
Rookirk <hwu24110@gmail.com>
2020-05-24 18:31:36 -0700

    Put in building 1 for Grid

---
+++ lab8/Procedural City Generation/Assets/Scripts/Building.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0.0f, 90.0f * Random.Range(0,3), 0.0f);
+    }
+}
